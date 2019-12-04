a3ab681ae8da81a2f0890d9499512cde2582a3ea
Md. Rejaul Karim <rktuhinbd@gmail.com>
2019-12-05 01:56:48 +0600

    Add transition animation effect when changing activity, Add all activities and click listener to them

---
+++ app/src/main/java/com/rktuhinbd/monthlyexpenses/Activity/RentActivity.java
@@ -0,0 +1,16 @@
+package com.rktuhinbd.monthlyexpenses.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rktuhinbd.monthlyexpenses.R;
+
+public class RentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rent);
+    }
+}
